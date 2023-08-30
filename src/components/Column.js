2eb9a1e5b89e18802888616dc4e20c3d2dcecc91
Kwasi O-T <tweneboah21@gmail.com>
2023-08-30 21:32:21 +0000

    completed styling for mobile view

--- src/components/Column.js
+++ src/components/Column.js
@@ -4,9 +4,9 @@ function Column({image, title, text, button}) {
     return (
         <div className="column">
             <div>{image}</div>
-            <div><h1>{title}</h1></div>
+            <div><h2>{title}</h2></div>
             <div><p>{text}</p></div>
-            <div>{button}</div>
+            <div className="button">{button}</div>
         </div>
     );
 }
